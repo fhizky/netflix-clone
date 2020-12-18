78bfc0babb5036e9a5bc0b9e22a0e7a8c26335ff
Fhizky Prasetio <fhizkyprasetio@gmail.com>
2020-12-18 16:44:33 +0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import request from "./request";
+import Banner from "./Banner";
+import Nav from "./Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchURL={request.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchURL={request.fetchTrending} />
+      <Row title="Top Rated" fetchURL={request.fetchTopRated} />
+      <Row title="Action Movies" fetchURL={request.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchURL={request.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchURL={request.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchURL={request.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchURL={request.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default App;
